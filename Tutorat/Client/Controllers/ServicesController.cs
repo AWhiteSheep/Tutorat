a59c1568eb7c92846deae0ed0f769d134f91b0d7
alex <cote.alexandre.0@gmail.com>
2019-12-18 13:01:57 -0500

    Send message on apply

--- Tutorat/Client/Controllers/ServicesController.cs
+++ Tutorat/Client/Controllers/ServicesController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Client.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Exchange.WebServices.Data;
+using System.Net.Mail;
+using Microsoft.Exchange.WebServices.Autodiscover;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Client.Controllers
 {
@@ -74,6 +78,7 @@ namespace Client.Controllers
         // POST: Services/Apply
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Apply([Bind("IdentifiantUtilisateur,IdentifiantHoraire")] Demandes application)
         {
             // rapporte le premier horaire afin de vérifier si l'application est vraisemblable
@@ -92,15 +97,69 @@ namespace Client.Controllers
                 // ajoute l'application
                 _context.Add(application);
                 // sauvegarde et redirige
+
                 await _context.SaveChangesAsync();
+
+
+                await _context.Entry(application).Reference(a => a.IdentifiantUtilisateurNavigation)
+                                            .LoadAsync();
+
+                await _context.Entry(application).Reference(a => a.IdentifiantHoraireNavigation)
+                                                .LoadAsync();
+
+                await _context.Entry(application.IdentifiantHoraireNavigation)
+                                    .Reference(h => h.Service)
+                                    .LoadAsync();
+
+                SendApplicationEmail(application);
+
                 return RedirectToAction(nameof(Index));
             }
 
             return Problem();
         }
 
-        // GET: Services/Details/5
-        public async Task<IActionResult> EditHoraire(int? id)
+        private void SendApplicationEmail(Demandes application)
+        {
+            ExchangeService myservice = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
+            myservice.Credentials = new WebCredentials();
+
+            string message = $"Confirmation d'inscription -- {application.IdentifiantHoraireNavigation.Service.ServiceTypeCode}; {System.Environment.NewLine}";
+
+            string emailDestination = application.IdentifiantUtilisateurNavigation.Email;
+
+            try
+            {
+                string serviceUrl = "https://outlook.office365.com/ews/exchange.asmx";
+
+                myservice.Url = new Uri(serviceUrl);
+                EmailMessage emailMessage = new EmailMessage(myservice);
+                emailMessage.Subject = "Test Subject";
+                emailMessage.Body = new MessageBody(message);
+
+                emailMessage.ToRecipients.Add(emailDestination);
+                emailMessage.Send();
+                Console.Write("message envoyé");
+                Console.ReadKey();
+            }
+            catch (SmtpException exception)
+            {
+                string msg = "Erreur:message n'a pas pu être envoyé";
+                msg += exception.Message;
+                throw new Exception(msg);
+            }
+
+            catch (AutodiscoverRemoteException exception)
+            {
+                string msg = "Erreur:message n'a pas pu être envoyé";
+                msg += exception.Message;
+                throw new Exception(msg);
+
+            }
+        }
+
+            // GET: Services/Details/5
+            public async Task<IActionResult> EditHoraire(int? id)
         {
             if (id == null)
             {
