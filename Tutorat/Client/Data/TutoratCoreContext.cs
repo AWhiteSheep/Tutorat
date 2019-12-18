618f1c8e7ae4b9a91714eec503de80b79aedfc80
awhitesheep <yanharc@gmail.com>
2019-12-18 09:47:12 -0500

    un stashed commit

--- Tutorat/Client/Data/TutoratCoreContext.cs
+++ Tutorat/Client/Data/TutoratCoreContext.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Client.Data;
 
 namespace Client.Data
 {
