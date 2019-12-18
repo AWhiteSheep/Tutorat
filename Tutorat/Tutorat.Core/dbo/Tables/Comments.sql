02b8401555877bba4068e7a669fe011f5136f51f
alex <cote.alexandre.0@gmail.com>
2019-12-18 09:02:49 -0500

    Comments table, added type code to services@

---
+++ Tutorat/Tutorat.Core/dbo/Tables/Comments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Comments]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [PosterId] INT NOT NULL, 
+    [ServiceId] INT NOT NULL, 
+    [PostText] NTEXT NOT NULL , 
+    [PostedDateTime] TIMESTAMP NOT NULL DEFAULT getdate()
+)
--- Tutorat/Tutorat.Core/dbo/Tables/Services.sql
+++ Tutorat/Tutorat.Core/dbo/Tables/Services.sql
@@ -3,6 +3,8 @@
     [TuteurId]    NVARCHAR (450) NOT NULL,
     [Titre]       NVARCHAR (MAX) NOT NULL,
     [Description] TEXT           NOT NULL,
+    [Price] MONEY NULL, 
+    [ServiceTypeCode] INT NULL DEFAULT 1, 
     CONSTRAINT [PK__Services__796424B83E787365] PRIMARY KEY CLUSTERED ([IdentityKey] ASC),
     CONSTRAINT [FK_Services_AspNetUsers] FOREIGN KEY ([TuteurId]) REFERENCES [dbo].[AspNetUsers] ([Id]) NOT FOR REPLICATION
 );
